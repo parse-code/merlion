d4a749490917fd91e6bf35e6f0537e4e50be5b6a
Alex <alex@parse.cn>
2025-07-03 13:02:33 +0800

    wip

--- resources/views/components/widgets/alert.blade.php
+++ resources/views/components/widgets/alert.blade.php
@@ -1,9 +1,4 @@
 @if($content = panel()->getAlert())
-    admin().toast({
-    text: "{{$message}}",
-    className: "{{session()->get('toast.type', 'success')}}",
-    position: '{{session()->get('toast.position', 'center')}}'
-    })
     @php
         $type = session()->get('alert.type', 'success');
     @endphp
