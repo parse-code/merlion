e5f6b4c4218184a916248613d9512658aadd7af6
Alex <alex@parse.cn>
2025-07-03 13:05:45 +0800

    wip

--- resources/views/components/widgets/alert.blade.php
+++ resources/views/components/widgets/alert.blade.php
@@ -4,5 +4,6 @@
     @endphp
     <div class="alert alert-{{$type}}">
         {{$content}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
