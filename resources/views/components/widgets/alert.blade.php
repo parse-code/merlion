372d45acda64c72e61787ab757d29f145e3214c9
Alex <alex@parse.cn>
2025-07-03 14:03:30 +0800

    wip

--- resources/views/components/widgets/alert.blade.php
+++ resources/views/components/widgets/alert.blade.php
@@ -2,7 +2,7 @@
     @php
         $type = session()->get('alert.type', 'success');
     @endphp
-    <div class="alert alert-{{$type}}">
+    <div class="alert alert-{{$type}} alert-dismissible fade show">
         {{$content}}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
