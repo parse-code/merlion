22c9fdc7cdd6a17613fc9190663170974885e989
Alex <alex@parse.cn>
2025-07-04 14:30:07 +0800

    wip

--- src/Components/Concerns/Panel/HasMenus.php
+++ src/Components/Concerns/Panel/HasMenus.php
@@ -5,6 +5,9 @@ namespace Merlion\Components\Concerns\Panel;
 trait HasMenus
 {
 
+    const MENU_MAIN     = 'menus';
+    const MENU_TOP_USER = 'menus_top_user';
+
     public function clearMenus($position = 'menus'): static
     {
         return $this->clear($position);
